94302f644795720a9bedbf9bfe7c39a38099d801
Eric Whited <whited.j.eric@gmail.com>
2018-11-06 16:45:30 -0500

    added email, github, and linkedIn links / icons

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,17 @@
-function scrolling() {
-    window.scrollTo('#projectSection')
-}
+var aboutLink = document.getElementById('aboutLink');
+var contactLink = document.getElementById('contactLink');
+var projectLink = document.getElementById('projectLink');
+
+var aboutPage = document.getElementById('about');
+// var contactPage = document.getElementById('contact');
+var projectPage = document.getElementById('projectSection');
+
+
+
+aboutLink.addEventListener('click', function(){
+    aboutPage.scrollIntoView({behavior: 'smooth'});
+})
+
+projectLink.addEventListener('click', function(){
+    projectPage.scrollIntoView({behavior: 'smooth'});
+})
