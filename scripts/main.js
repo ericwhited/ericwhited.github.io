f8941c84f2f1e40e35a88cfd403e15aa41aad54e
=Eric Whited <eejw93@gmail.com>
2018-11-13 15:42:53 -0500

    added navShow until pop out fix

--- scripts/main.js
+++ scripts/main.js
@@ -26,3 +26,26 @@ var downArrow = document.getElementById('downIcon')
 downArrow.addEventListener('click', function(){
     aboutPage.scrollIntoView({behavior: 'smooth'});
 })
+
+
+// nav pop out 
+// var navBox = document.getElementById('navBox');
+// var nav = document.getElementById('nav');
+
+// navBox.addEventListener('mouseover', function(){
+//     nav.classList.add("navShow")
+// });
+
+
+// navBox.addEventListener('mouseleave', function(){
+//     nav.classList.remove('navShow')
+// });
+
+
+
+// document.onmouseover = function (e) {
+//     console.log(e.target);
+//     if(e.target !=homeLink || e.target !=projectLink || e.target!=aboutLink || e.target!=navBox || e.target !=nav) {
+//         nav.classList.remove('navShow')
+//     }
+// }
